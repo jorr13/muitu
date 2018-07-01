2f67409f67b389417df4eab24dd691d6e743f2b5
jorr13 <jerinson1996@gmail.com>
2018-07-01 10:31:32 +0200

    contactanos terminado

--- web/app/themes/muitu/resources/views/contactanos.blade.php
+++ web/app/themes/muitu/resources/views/contactanos.blade.php
@@ -16,12 +16,13 @@
         {{-- loop through the rows of data --}}
         @while (have_rows('tarjeta', 97))@php(the_row())
             <div class="tarjetauna column is-4">
+                <div class="fondoimg"></div>
                 <div class="containertarjeta">
                     <h1 class="name">{{ get_sub_field('name') }}</h1>
                     <p class="cargo">{{ get_sub_field('cargo') }}</p>
                     <p class="correo">{{ get_sub_field('correo_electronico') }}</p>
+                    <p class="dominio">{{ get_sub_field('dominio') }}</p>
                     <h1 class="telefono">{{ get_sub_field('numero_telefonico') }}</h1>
-
                 </div>
             </div>      
         @endwhile 
